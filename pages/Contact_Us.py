9950e9e6e7a778d277d9bfc7f553859addb2e6f9
EricLod <eric.wiehman@lodicon.co.za>
2023-01-11 13:14:58 +0200

    Day 23: .gitignore

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,19 +1,24 @@
 import streamlit as st
 from send_email import send_email
+import pandas
+
+df = pandas.read_csv("topics.csv")
 
 st.header("Contact Me")
 
 with st.form(key="email_form"):
     user_email = st.text_input("Your Email Address")
+    drop_menu = st.selectbox("What topic do you want to discuss", (df["topic"]))
     text_area_message = st.text_area("Your Message")
-    message = f"""\
-    Subject: New email from {user_email}
-    
-    {text_area_message}
-    
-    From: {user_email}
-    """
+    message =f"""\
+Subject: ({drop_menu}) New email from {user_email}
+
+{text_area_message}
+
+From: {user_email}
+"""
     button = st.form_submit_button("Send")
     if button:
+        print(message)
         send_email(message)
         st.info("Your email was sent successfully!")
--- send_email.py
+++ send_email.py
@@ -12,13 +12,6 @@ def send_email(message):
     receiver = "jew.user@gmail.com"
     context = ssl.create_default_context()
 
-    #message = """\
-    #Subject: New email from asdf@asdf.com
-    
-    #From: asdf@asdf.com
-    #asdfjksdla;woeifjaweofj
-    #"""
-
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, msg)
